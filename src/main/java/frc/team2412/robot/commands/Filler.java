614c709cccf9fc8215d951c537415c71a04daa00
SumedhP <43325361+SumedhP@users.noreply.github.com>
2021-01-25 15:35:17 -0800

    Fresh start for Heron Successor

---
+++ src/main/java/frc/team2412/robot/commands/Filler.java
@@ -0,0 +1,5 @@
+package frc.team2412.robot.commands;
+
+public class Filler {
+
+}
---
+++ src/test/java/frc/team2412/robot/subsystems/Filler.java
@@ -0,0 +1,7 @@
+package frc.team2412.robot.subsystems;
+
+
+// This is an example test of the robot. This is to make sure that everything is working as intended before code goes on a robot.
+public class Filler  {
+
+}
